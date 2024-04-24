b6240072804415ccde905d5df9deef674cbf3236
Benjamin Maggi <benjamin.maggi@fravega.com.ar>
2024-04-24 08:56:39 -0300

    chore: app skeleton

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  delay(1000);
+}
